d59e7327ec4b59eda7be95e2b729dbab49b66c88
Wonbin Jin <wbjin@umich.edu>
2025-04-24 17:20:18 -0400

    log

--- domain_finder.py
+++ domain_finder.py
@@ -22,6 +22,7 @@ def extract_url_if_matches(file_path: str, base_url: str):
                     extracted_url = line[start_pos:end_pos]
                     
                     if extracted_url.startswith(base_url):
+                        print("Deleteing", file_path)
                         os.remove(file_path)
                         return True
                     return False
